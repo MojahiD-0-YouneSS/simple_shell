6857a715ada63434a5667b0c7cb964d856e7c853
youness mojahid <mojahidyouness0@gmail.com>
2023-12-13 14:56:54 -0800

    shell 1.0

---
+++ shell0_1.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 100
+
+void execute_command(const char *command) {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        /* Child process */
+        char **args = (char **)malloc(2 * sizeof(char *));
+        if (execve(command, args, NULL) == -1) {
+            perror("Error executing command");
+            exit(EXIT_FAILURE);
+        }
+    } else if (pid > 0) {
+        /* Parent process */
+        int status;
+        waitpid(pid, &status, 0);
+    } else {
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main() {
+    char command[MAX_COMMAND_LENGTH];
+
+    while (1) {
+        /* Display prompt */
+        printf("$ ");
+        fflush(stdout);
+
+        /* Read the command */
+        if (fgets(command, sizeof(command), stdin) == NULL) {
+            printf("\nExiting the shell. Goodbye!\n");
+            break;
+        }
+
+        /* Remove the newline character */
+        if (command[strlen(command) - 1] == '\n') {
+            command[strlen(command) - 1] = '\0';
+        }
+
+        /* Execute the command */
+        execute_command(command);
+    }
+
+    return 0;
+}
+
