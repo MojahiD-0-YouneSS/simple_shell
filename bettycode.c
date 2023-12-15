a6eb6597a1647fa03d7a544348136f0b38893fab
youness mojahid <mojahidyouness0@gmail.com>
2023-12-15 08:55:46 -0800

    heros die in battel

---
+++ bettycode.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("Hello, Betty!\n");
+    }
+
+    return 0;
+}
+
